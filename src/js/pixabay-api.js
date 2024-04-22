4120ebf531cda98ce26c051c1742ca66a6f5b06a
Rimma1992 <149500783+Rimma1992@users.noreply.github.com>
2024-04-22 09:05:02 +0200

    updATE

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,31 @@
-export function searchImages(query) {
-  const BASE_URL = 'https://pixabay.com/';
-  const END_POINT = 'api/';
-  const params = new URLSearchParams({
-    key: '42797390-82869f1936654cf50378828df',
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  
-  const url = `${BASE_URL}${END_POINT}?${params}`;
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-  return fetch(url).then(res => {
-    if (!res.ok) throw new Error(res.status);
-    return res.json();
-  });
+const loader = document.querySelector('.loader');
+const KEY = '43226276-a07a0c17e428cfffb021b9b05';
+const BASE_URI = 'https://pixabay.com/api/';
+
+export function getImages(QUERY) {
+  loader.style.display = 'flex';
+  const LINK = `${BASE_URI}?key=${KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  return fetch(LINK)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
